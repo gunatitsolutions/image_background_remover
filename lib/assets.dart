8554aaac1d70cbae5cfc4fa0b068ef97eb8ae7d7
ravilakhtariya <gunatitsolutions@gmail.com>
2025-05-13 18:04:35 +0530

    code updated

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,4 +1,7 @@
 class Assets {
   static const modelPath =
       'packages/image_background_remover/assets/model.onnx';
+
+  static const modNetPath =
+      'packages/image_background_remover/assets/modnet.onnx';
 }
